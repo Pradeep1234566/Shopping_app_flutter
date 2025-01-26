c3f3b0510bb23e78778049ce78629a416a9bde5b
Pradeep <pawarpradeep1707@gmail.com>
2025-01-26 12:14:28 +0530

    HomePage

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -5,6 +5,49 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 20.0, top: 15.0),
+                  child: Icon(
+                    Icons.menu,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(right: 20.0, top: 15.0),
+                  child: Container(
+                    width: 40,
+                    height: 40,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      image: DecorationImage(
+                        image: AssetImage('assest/Images/P1.png'),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Align(
+                alignment: Alignment.centerLeft,
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 20.0, top: 15.0),
+                  child: Text(
+                    'Our',
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ))
+          ],
+        ),
+      ),
+    );
   }
 }
